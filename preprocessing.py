7853de8deb4e2c06fea59652d3772284a130feea
David Robinson <drobins4@swarthmore.edu>
2025-10-30 10:53:54 +0800

    Add feature scaling function

---
+++ preprocessing.py
@@ -0,0 +1,36 @@
+# Define functions to prepare chemical abundance calculations for input to ML models
+
+# Import statements
+import torch
+
+# Rescale log(abundances) and log(T) to [-1, 1] interval
+def scale_features(input_tensor):
+    '''
+    Inputs:
+    input_tensor (torch.tensor): contains times, chemical abundances, and temperatures from solved chemical network
+    Outputs:
+    feature_tensor (torch.tensor): contains times, rescaled chemical abundances and temperatures
+    '''
+
+    # Create tensor to hold features
+    feature_tensor = torch.zeros_like(input_tensor)
+    # First column will still be times (these are not scaled)
+    feature_tensor[:, 0] = input_tensor[:, 0]
+
+    # Get number of columns in input_tensor
+    num_cols = input_tensor.size()[-1]
+
+    # Loop through abundance, temperature columns
+    for i in range(1, num_cols):
+        # Get the elements of the column, take logarithm
+        col = torch.log10(input_tensor[:, i])
+
+        # Get min and max values of log(abundance) or log(T)
+        max = torch.max(col)
+        min = torch.min(col)
+
+        # Linearly rescale col to the interval [-1, 1]
+        feature_tensor[:, i] = -1 + 2 * (col - min) / (max - min)
+
+    # Return the tensor containing the features
+    return feature_tensor
