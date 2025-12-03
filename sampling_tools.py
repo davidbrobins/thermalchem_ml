060a8cf787f2cb433231a57c675b24222a479fe1
davidbrobins <dbrobins@umich.edu>
2025-12-03 18:33:05 +0800

    Framework to write file with sampled initial conditions

--- sampling_tools.py
+++ sampling_tools.py
@@ -3,7 +3,8 @@
 
 # Import statements
 from scipy.stats import qmc # Multi-dimensional quasi-Monte Carlo sampling from Scipy
-import numpy as np
+import numpy as np # Numpy for math
+import pandas as pd # Pandas for dataframe handling
 
 # Function to generate physical parameters file for Chempl from sampled parameters
 def generate_phy_params_file(T_gas, n_gas, G0_UV, f_H2, cell_thickness_pc, file_path):
@@ -12,9 +13,11 @@ def generate_phy_params_file(T_gas, n_gas, G0_UV, f_H2, cell_thickness_pc, file_
     T_gas (float): Gas temperature [K]
     n_gas (float): Gas (hydrogen) number density [cm^-3]
     G0_UV (float): Radiation field strength (in units of Draine field)
-    f_H2 (float): Fraction of hydrogen in the form of H2 (in the interval [0, 1])
+    f_H2 (float): Initial fraction of hydrogen in the form of H2 (in the interval [0, 1])
     cell_thickness_pc (float): Cell thickness [pc]
     file_path (str): Path to the file where the physical parameters will be written (must include the file name)
+    Outputs:
+    None (writes file containing physical parameters for chempl run to file_path)
     '''
 
     # Calculate derived parameters
@@ -46,3 +49,32 @@ def generate_phy_params_file(T_gas, n_gas, G0_UV, f_H2, cell_thickness_pc, file_
     f.write('Av = ' + str(A_V) + '\n') # Write dust extinction
     f.write('T_dust = ' + str(T_dust) + '\n') # Write dust temperature
     f.close() # Close the file
+
+# Function to generate ICs file for Chempl from the sampled parameters
+def generate_ics_file(f_H2, x_N, x_C, x_O, x_S, x_Si, x_Na, x_Mg, x_Fe, x_P, x_F, x_Cl, file_path):
+    '''
+    Inputs:
+    f_H2 (float): Initial fraction of hydrogen in (neutral) molecular form (in the interval [0, 1])
+    x_N (float): Initial abundance of nitrogen (all abundances x_i = n_i/n_H)
+    x_C (float): Initial abundance of carbon
+    x_O (float): Initial abundance of oxygen
+    x_S (float): Initial abundance of sulfur
+    x_Si (float): Initial abundance of silicon
+    x_Na (float): Initial abundance of sodium
+    x_Mg (float): Initial abundance of magnesium
+    x_Fe (float): Initial abundance of iron
+    x_P (float): Initial abundance of phosphorus
+    x_F (float): Initial abundance of flourine
+    x_Cl (float): Initial abundance of chlorine
+    file_path (str): Path to the file where the physical parameters will be written (must include the file name)
+    Outputs:
+    None (writes file containing physical parameters for chempl run to file_path)
+    '''
+
+    # Package all abundances as a pandas Series 
+    abundances = pd.Series({'H2': f_H2, # Helium?
+                            'N': x_N, 'C': x_C, # Do any of the ions need to be charged?
+                            })
+    # Write the abundances to a file
+    abundances.to_string(buf = file_path, header = False)
+
