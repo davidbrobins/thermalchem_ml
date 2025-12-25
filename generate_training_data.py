fac573a12c0598adbb4bd294a1fc0c0306b952f0
davidbrobins <davidbenjaminrobinson@gmail.com>
2025-12-25 11:35:57 +0800

    Fix chempl run location bug

--- generate_training_data.py
+++ generate_training_data.py
@@ -12,11 +12,12 @@ import numpy as np # Numpy for math
 # Unpack command line arguments
 # Name of this file, directory where samples will be stored, number of samples, number of samples generated previously
 (pyfilename, samples_dir, num_samples, prev_samples) = sys.argv 
+# Convert num_samples,, prev_samples to integers
+num_samples = int(num_samples)
+prev_samples = int(prev_samples)
 
 # Run the sampling
 samples = sampling_tools.sampling(num_samples, samples_dir, prev_samples)
-# Print to check
-print(samples)
 
 # Define a function to run chempl on a given sample index
 def run_chempl(sample_index):
@@ -27,15 +28,13 @@ def run_chempl(sample_index):
     None
     '''
 
-    # Change working location to chempl directory (modify as needed for your working setup)
-    subprocess.run(['cd', '../chempl'])
     # Get location of run file containing locations of needed parameter files for chempl
     run_file = samples_dir + str(sample_index).zfill(6) + '/run_file.dat'
     print('Run file being used: ', run_file)
-    # Run chempl on run_file
-    subprocess.run(['./re', run_file], capture_output = True)
+    # Run chempl on run_file, changing run location to chempl location (modify as needed for your working tree setup)
+    subprocess.run(['./re', run_file], capture_output = True, cwd = '~/code/chempl/')
 
-# Run chempl in parallel across 8 processors
+# Run chempl in parallel across multiple processors (here, 8)
 if __name__ == "__main__": 
     # Set up the parallel processing
     pool = Pool(processes = 8) # Modify as needed for your system
